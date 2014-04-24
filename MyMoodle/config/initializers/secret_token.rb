a03eff9399abc87af7f56f53e067fbaaea79b461
souffletmarc <marc.soufflet@epitech.eu>
2014-04-24 15:51:26 +0100

    Base Project

---
+++ MyMoodle/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyMoodle::Application.config.secret_key_base = 'ab59dbbee50e8aaa0929302670a475f7aeec74886e77bd45ffd522e482f311cd5f594591adb5f76d8fa31c936ca7fef1713054435e36bd42336476a170cd0ceb'
