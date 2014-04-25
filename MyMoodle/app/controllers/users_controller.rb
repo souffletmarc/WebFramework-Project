cd02abe80b4d8814fa2b22efa451739aa4ce4017
Cynt <cyntia.marquis@epitech.eu>
2014-04-25 18:24:51 +0100

    tg2

--- MyMoodle/app/controllers/users_controller.rb
+++ MyMoodle/app/controllers/users_controller.rb
@@ -25,11 +25,14 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
+
+    respond_to do |format|
       if @user.save
-      redirect_to @user, notice: 'User was successfully created.'ser 
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @user }
       else
-        render action: 'new' 
-        render json: @user.errors, status: :unprocessable_entity
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
